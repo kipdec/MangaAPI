ed260620ea370f0004e25f41d0fa86f26a00cfbe
Kip DeCastro <kipdec@gmail.com>
2022-12-01 19:19:47 -0600

    Initial commit

---
+++ Console/console.py
@@ -0,0 +1,20 @@
+# Connect to DB
+# Get all the manga titles and return to the user asking which title they would like to learn more information about
+# Return information about their request
+
+# EXAMPLE:
+
+# Here are the volumes in the DB:
+# 1. Dragon Ball
+# 2. Bleach
+# ...
+# Which would you like to learn more about?
+#
+# User enter 2
+#
+# Bleach Volume 1 came out in some year and was written by somebody
+# Would you like to open the images?
+#
+# User enters Y
+#
+# Program opens the folder containing the images
---
+++ readme.md
@@ -0,0 +1,4 @@
+# TODO
+
+- [ ] Have console connect to db
+- [ ] Have console list all the manga
